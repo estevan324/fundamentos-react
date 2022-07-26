974435bf596a8ea49cc22f442a578959caedc714
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2022-07-26 20:48:37 -0300

    Alteração no arquivo index.js: Utilização do JSX para a utilização de marcações HTML dentro do projeto

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,4 +1,12 @@
 import ReactDom from 'react-dom'
+import React from 'react'
 
-const el = document.getElementById('root')
-ReactDom.render('Olá React', el)
+const elem = document.getElementById('root')
+const tag = <strong>Olá React!</strong>
+
+ReactDom.render(
+    <div>
+        { tag }
+    </div>, 
+    elem
+)
