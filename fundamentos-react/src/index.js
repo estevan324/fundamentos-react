d22eb3b22585e3345717e49ab1462478458c3d3c
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2022-07-26 20:55:25 -0300

    Criação do arquivo index.css: adicionado o estilo na propriedade body; Alteração no arquivo index.js: importação do arquivo de estilos index.css

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,4 +1,7 @@
+import './index.css'
 import ReactDom from 'react-dom'
+
+// Na nova versão do React, não necessita importar o React para renderização de JSX
 import React from 'react'
 
 const elem = document.getElementById('root')
