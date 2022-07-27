5c02d55741eef63374668c63708838d8c9d20f75
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2022-07-26 21:50:43 -0300

    Criação do componente Fragmento: utilização do React.Fragment para resolução de um erro de envolvimento de tags HTML

---
+++ fundamentos-react/src/components/basicos/Fragmento.jsx
@@ -0,0 +1,8 @@
+export default function Fragmento(){
+    return(
+        <>
+            <h3>Fragmento</h3>
+            <p>Cuidado com esse erro!</p>
+        </>
+    )
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDom.render(
     <div>
@@ -12,6 +13,7 @@ ReactDom.render(
             nome="João Carlos"
             nota={6}
         />
+        <Fragmento/>
     </div>, 
     document.getElementById('root')
 )
