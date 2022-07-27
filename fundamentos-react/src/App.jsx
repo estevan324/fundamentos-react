7bc7371414caddfb22c6f21fb8e000a44fecf66d
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2022-07-27 20:24:36 -0300

    Estilização do componente Card utilizando parâmetros para alterar o estilo css da classe .card

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,3 +1,5 @@
+import './App.css'
+
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
@@ -6,27 +8,30 @@ import Card from './components/layout/Card'
 
 export default () => {
     return (
-        <div id="app">
+        <div className='app'>
             <h1>Fundamentos React</h1>
-            <Card titulo='#04 - Desafio Aleatório'>
-                <Aleatorio min={0} max={200} />
-            </Card>
+            <div className='cards'>
 
-            <Card titulo='#03 - Erro: React Fragment'>
-                <Fragmento />
-            </Card>
+                <Card titulo='#04 - Desafio Aleatório' color='#FA6900'>
+                    <Aleatorio min={0} max={200} />
+                </Card>
 
-            <Card titulo='#02 - Boletim Escolar (com parâmetro)'>
-                <ComParametro
-                    titulo="Situação do Aluno"
-                    nome="João Carlos"
-                    nota={6}
-                />
-            </Card>
+                <Card titulo='#03 - Erro: React Fragment' color='#E94C6F'>
+                    <Fragmento />
+                </Card>
 
-            <Card titulo='#01 - Criação do Primeiro Componente'>
-                <Primeiro></Primeiro>
-            </Card>
+                <Card titulo='#02 - Boletim Escolar (com parâmetro)' color='#E8B71A'>
+                    <ComParametro
+                        titulo="Situação do Aluno"
+                        nome="João Carlos"
+                        nota={6}
+                    />
+                </Card>
+
+                <Card titulo='#01 - Criação do Primeiro Componente' color='#588C73'>
+                    <Primeiro></Primeiro>
+                </Card>
+            </div>
         </div>
     )
 }
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -1,8 +1,12 @@
 import './Card.css'
 
 export default props => {
+    const cardStyle = {
+        backgroundColor: props.color || '#008',
+        borderColor: props.color || '#008'
+    }
     return (
-        <div className='card'>
+        <div className='card' style={cardStyle}>
             <div className='title'>{ props.titulo }</div>
             <div className='content'>
                 { props.children }
